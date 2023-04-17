960618bbef4dce0687ff8a0135052e2f8a8247b8
lukeyusuke <lukedev09@gmail.com>
2023-04-17 14:51:29 -0300

    Cria função getAllBreeds
    - Função que pega todas as raças de cães disponíveis
    na API;

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
-const url = "https://api.thedogapi.com/v1/breeds/53";
+const url = "https://api.thedogapi.com/v1/breeds/";
 const apiKey = "live_anHbEgCLTK9xBPpYclvf2LFsSq8T8WkTrFxIXeJAzlv6pfsGzCZlR6PhJFKndxwY";
 
-const requestValues = {
+const sendValues = {
     method: 'GET',
     headers: {
         'Content-type': 'application/json',
         'x-api-key': `${apiKey}`
     },
 }
+
+const getAllBreeds = async() => {    
+    const response = await fetch(url, sendValues);
+    const data = await response.json();
+}
+
+getAllBreeds();
