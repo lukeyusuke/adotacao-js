7c64c71f2110a9ec80d8e60643e979047f5b7e49
lukeyusuke <lukedev09@gmail.com>
2023-04-18 14:22:44 -0300

    Cria função showBreeds
    - Função criada para puxar dados dos cães, como nome e imagem e
    exibir na tela.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
-const url = "https://api.thedogapi.com/v1/breeds/";
-const apiKey = "live_anHbEgCLTK9xBPpYclvf2LFsSq8T8WkTrFxIXeJAzlv6pfsGzCZlR6PhJFKndxwY";
+const apiIntegration = () => {
+    const apiData = () => {
+        const apiKey = "live_anHbEgCLTK9xBPpYclvf2LFsSq8T8WkTrFxIXeJAzlv6pfsGzCZlR6PhJFKndxwY";
+    
+        const sendValues = {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+                'x-api-key': `${apiKey}`
+            },
+        }
 
-const sendValues = {
-    method: 'GET',
-    headers: {
-        'Content-type': 'application/json',
-        'x-api-key': `${apiKey}`
-    },
+        getAllBreeds(sendValues);
+    }
+    
+    const getAllBreeds = async(sendValues) => {    
+        const response = await fetch("https://api.thedogapi.com/v1/breeds/", sendValues);
+        const data = await response.json();
+    
+        showBreeds(data);
+    }
+    
+    showBreeds = (data) => {
+        const dogData = data.map((dog) => {
+            console.log(dog.name);
+            console.log(dog.image.url);
+        })
+    }
+
+    getAllBreeds();
 }
 
-const getAllBreeds = async() => {    
-    const response = await fetch(url, sendValues);
-    const data = await response.json();
-}
+apiIntegration();
 
-getAllBreeds();
